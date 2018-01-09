359951291b6c0c45637e50ba01f21bb5e8621c13
Volker Stuber <stuber@leadingsystems.de>
2018-01-09 14:00:54 +0100

    use composer autoloading for functions.php and do not try to require the functions.php in the config.php

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -2,7 +2,6 @@
 
 namespace LeadingSystems\Helpers;
 
-require_once(__DIR__.'/../functions.php');
 ls_toggleLogClass();
 
 $GLOBALS['TL_HOOKS']['replaceInsertTags'][] = array('LeadingSystems\Helpers\ls_helpers_customInserttags', 'customInserttags');
