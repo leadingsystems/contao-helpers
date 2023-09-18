8daaa939fcf3992ce0679c000db464dd78f0692c
Daniel Bitsch <bitsch@leadingsystems.de>
2023-09-18 13:41:47 +0200

    optimize codestyling

--- src/Resources/contao/dca/tl_ls_helpers_segmentizer.php
+++ src/Resources/contao/dca/tl_ls_helpers_segmentizer.php
@@ -6,8 +6,9 @@ use Contao\DC_Table;
 
 $GLOBALS['TL_DCA']['tl_ls_helpers_segmentizer'] = array
 (
-    'config' => array(
-        'dataContainer' => DC_Table::class,
+    'config' => array
+    (
+        'dataContainer'               => DC_Table::class,
         'sql' => array
         (
             'keys' => array
@@ -16,40 +17,40 @@ $GLOBALS['TL_DCA']['tl_ls_helpers_segmentizer'] = array
             )
         )
 
-	),
-	'fields' => array
+    ),
+    'fields' => array
     (
         'segmentationToken' => array
         (
             'sql'                     => "varchar(255) NOT NULL default ''"
-		),
+        ),
         'info' => array
         (
             'sql'                     => "varchar(255) NOT NULL default ''"
-		),
+        ),
         'tstampLastCall' => array
         (
-		    'sql'                     => "int(10) unsigned NOT NULL default '0'"
-		),
+            'sql'                     => "int(10) unsigned NOT NULL default '0'"
+        ),
         'tstampExpiration' => array
         (
-		    'sql'                     => "int(10) unsigned NOT NULL default '0'"
-		),
+            'sql'                     => "int(10) unsigned NOT NULL default '0'"
+        ),
         'numSegmentsTotal' => array
         (
-		    'sql'                     => "int(10) unsigned NOT NULL default '0'"
-		),
+            'sql'                     => "int(10) unsigned NOT NULL default '0'"
+        ),
         'lastSegment' => array
         (
-		    'sql'                     => "int(10) unsigned NOT NULL default '0'"
-		),
+            'sql'                     => "int(10) unsigned NOT NULL default '0'"
+        ),
         'currentTurn' => array
         (
-		    'sql'                     => "int(10) unsigned NOT NULL default '1'"
-		),
+            'sql'                     => "int(10) unsigned NOT NULL default '1'"
+        ),
         'nextCallIsNewTurn' => array
         (
-		    'sql'                     => "char(1) unsigned NOT NULL default ''"
-		)
-	)
+            'sql'                     => "char(1) unsigned NOT NULL default ''"
+        )
+    )
 );
