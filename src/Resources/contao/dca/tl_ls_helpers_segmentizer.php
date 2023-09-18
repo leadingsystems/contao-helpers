e112502efef04eaed80e9fd52c6e5fd9ae23503e
Daniel Bitsch <bitsch@leadingsystems.de>
2023-09-18 13:03:15 +0200

    optimize codestyling

--- src/Resources/contao/dca/tl_ls_helpers_segmentizer.php
+++ src/Resources/contao/dca/tl_ls_helpers_segmentizer.php
@@ -4,9 +4,10 @@ namespace LeadingSystems\Helpers;
 
 use Contao\DC_Table;
 
-$GLOBALS['TL_DCA']['tl_ls_helpers_segmentizer'] = array(
-	'config' => array(
-		'dataContainer' => DC_Table::class,
+$GLOBALS['TL_DCA']['tl_ls_helpers_segmentizer'] = array
+(
+    'config' => array(
+        'dataContainer' => DC_Table::class,
         'sql' => array
         (
             'keys' => array
@@ -16,44 +17,36 @@ $GLOBALS['TL_DCA']['tl_ls_helpers_segmentizer'] = array(
         )
 
 	),
-
-	'fields' => array(
-
-		'segmentationToken' => array
+	'fields' => array
+    (
+        'segmentationToken' => array
         (
             'sql'                     => "varchar(255) NOT NULL default ''"
 		),
-
         'info' => array
         (
             'sql'                     => "varchar(255) NOT NULL default ''"
 		),
-
-		'tstampLastCall' => array
+        'tstampLastCall' => array
         (
 		    'sql'                     => "int(10) unsigned NOT NULL default '0'"
 		),
-
-		'tstampExpiration' => array
+        'tstampExpiration' => array
         (
 		    'sql'                     => "int(10) unsigned NOT NULL default '0'"
 		),
-
-		'numSegmentsTotal' => array
+        'numSegmentsTotal' => array
         (
 		    'sql'                     => "int(10) unsigned NOT NULL default '0'"
 		),
-
         'lastSegment' => array
         (
 		    'sql'                     => "int(10) unsigned NOT NULL default '0'"
 		),
-
         'currentTurn' => array
         (
 		    'sql'                     => "int(10) unsigned NOT NULL default '1'"
 		),
-
         'nextCallIsNewTurn' => array
         (
 		    'sql'                     => "char(1) unsigned NOT NULL default ''"
