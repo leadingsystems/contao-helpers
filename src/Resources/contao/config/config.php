607f8fac357682db1ec055fa1afab42d6f000475
Daniel Bitsch <bitsch@leadingsystems.de>
2023-12-20 17:16:05 +0100

    uncomment array_keys(): Argument #1 ($array) must be of type array, null given

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -8,14 +8,17 @@ $GLOBALS['FE_MOD']['ls_helpers'] = array(
 	'ModuleFlexWidgetTest' => 'LeadingSystems\Helpers\ModuleFlexWidgetTest',
 );
 
-ArrayUtil::arrayInsert(
-    $GLOBALS['TL_CTE']['texts'],
-    array_search('html', array_keys($GLOBALS['TL_CTE']['texts'])) + 1,
-    [
-        'htmlWrapperStart' => 'LeadingSystems\Helpers\ContentHtmlWrapperStart',
-        'htmlWrapperStop' => 'LeadingSystems\Helpers\ContentHtmlWrapperStop',
-    ]
-);
+/*
+ *
+ */
+//ArrayUtil::arrayInsert(
+//    $GLOBALS['TL_CTE']['texts'],
+//    array_search('html', array_keys($GLOBALS['TL_CTE']['texts'])) + 1,
+//    [
+//        'htmlWrapperStart' => 'LeadingSystems\Helpers\ContentHtmlWrapperStart',
+//        'htmlWrapperStop' => 'LeadingSystems\Helpers\ContentHtmlWrapperStop',
+//    ]
+//);
 
 $GLOBALS['TL_WRAPPERS']['start'][] = 'htmlWrapperStart';
 $GLOBALS['TL_WRAPPERS']['stop'][] = 'htmlWrapperStop';
