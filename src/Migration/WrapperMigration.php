03622cf7b54a182593234da3292421dde49b2799
Daniel Bitsch <bitsch@leadingsystems.de>
2024-05-23 17:49:51 +0200

    add migration

---
+++ src/Migration/WrapperMigration.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace LeadingSystems\HelpersBundle\Migration;
+
+use Composer\InstalledVersions;
+use Contao\CoreBundle\Migration\AbstractMigration;
+use Contao\CoreBundle\Migration\MigrationResult;
+use Doctrine\DBAL\Connection;
+
+class WrapperMigration extends AbstractMigration
+{
+    /**
+     * @var Connection
+     */
+    private $connection;
+
+    public function __construct(Connection $connection)
+    {
+        $this->connection = $connection;
+    }
+
+    public function shouldRun(): bool
+    {
+        $schemaManager = $this->connection->createSchemaManager();
+
+        if (!$schemaManager->tablesExist(['tl_content'])) {
+            return false;
+        }
+
+        if(version_compare(
+            InstalledVersions::getPrettyVersion('leadingsystems/lsce'),
+            "1.1.3",
+            '<'
+        )){
+            return false;
+        }
+
+        $test = $this->connection->fetchNumeric("
+            SELECT TRUE
+            FROM `tl_content` 
+            WHERE type='htmlWrapperStart' OR type='htmlWrapperStop'
+        ");
+
+        return false !== $test;
+    }
+
+    public function run(): MigrationResult
+    {
+        $this->connection->update('tl_content', ['type' => 'rsce_htmlwrapper-start'], ['type' => 'htmlWrapperStart']);
+
+        $this->connection->update('tl_content', ['type' => 'rsce_htmlwrapper-stop'], ['type' => 'htmlWrapperStop']);
+
+        return $this->createResult(true);
+    }
+}
