536159cdd0e307cec1e2dcb9ea1ce8781aaa5d2f
Daniel Bitsch <bitsch@leadingsystems.de>
2024-06-04 15:14:03 +0200

    port #19 (improve createMultidimensionalArray)

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -2,11 +2,16 @@
 
 namespace LeadingSystems\Helpers;
 
-use Contao\ArrayUtil;
+use Contao\System;
+use Symfony\Component\HttpFoundation\Request;
 
 $GLOBALS['FE_MOD']['ls_helpers'] = array(
 	'ModuleFlexWidgetTest' => 'LeadingSystems\Helpers\ModuleFlexWidgetTest',
 );
+
+if (System::getContainer()->get('contao.routing.scope_matcher')->isBackendRequest(System::getContainer()->get('request_stack')->getCurrentRequest() ?? Request::create(''))) {
+    $GLOBALS['LS_API_HOOKS']['apiReceiver_processRequest'][] = array('LeadingSystems\Helpers\ls_shop_apiController_findDoubleFlexContents', 'processRequest');
+}
 
 /*
  * @toDo Remove this Content Element. An LSCE is created as a replacement.
