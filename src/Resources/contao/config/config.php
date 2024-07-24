7197dc2ef7bf837c51ba109c959410270e9d5e32
Daniel Bitsch <bitsch@leadingsystems.de>
2024-07-24 13:33:03 +0200

    remove unnecessary code

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -12,19 +12,3 @@ $GLOBALS['FE_MOD']['ls_helpers'] = array(
 if (System::getContainer()->get('contao.routing.scope_matcher')->isBackendRequest(System::getContainer()->get('request_stack')->getCurrentRequest() ?? Request::create(''))) {
     $GLOBALS['LS_API_HOOKS']['apiReceiver_processRequest'][] = array('LeadingSystems\Helpers\ls_shop_apiController_findDoubleFlexContents', 'processRequest');
 }
-
-/*
- * @toDo Remove this Content Element. An LSCE is created as a replacement.
- * LSBoard Task 6349
- */
-//ArrayUtil::arrayInsert(
-//    $GLOBALS['TL_CTE']['texts'],
-//    array_search('html', array_keys($GLOBALS['TL_CTE']['texts'])) + 1,
-//    [
-//        'htmlWrapperStart' => 'LeadingSystems\Helpers\ContentHtmlWrapperStart',
-//        'htmlWrapperStop' => 'LeadingSystems\Helpers\ContentHtmlWrapperStop',
-//    ]
-//);
-//
-//$GLOBALS['TL_WRAPPERS']['start'][] = 'htmlWrapperStart';
-//$GLOBALS['TL_WRAPPERS']['stop'][] = 'htmlWrapperStop';
