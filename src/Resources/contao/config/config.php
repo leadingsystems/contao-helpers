59b1e8627090c7ae55819812c438b89bba3b2bb7
Daniel Bitsch <bitsch@leadingsystems.de>
2024-07-24 14:26:50 +0200

    fix some issues for Contao 5 (#15)
    * uncomment array_keys(): Argument #1 ($array) must be of type array, n…
    
    * fix import a library
    
    * remove fallback code for Contao < 3.0.0
    
    * htmlwrapper migration (#23)
    
    * port #19 (improve createMultidimensionalArray)
    
    * remove unnecessary code
    
    * rebase on 'develop'
    
    * update composer.json

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -2,24 +2,13 @@
 
 namespace LeadingSystems\Helpers;
 
-use Contao\ArrayUtil;
+use Contao\System;
+use Symfony\Component\HttpFoundation\Request;
 
 $GLOBALS['FE_MOD']['ls_helpers'] = array(
 	'ModuleFlexWidgetTest' => 'LeadingSystems\Helpers\ModuleFlexWidgetTest',
 );
 
-ArrayUtil::arrayInsert(
-    $GLOBALS['TL_CTE']['texts'],
-    array_search('html', array_keys($GLOBALS['TL_CTE']['texts'])) + 1,
-    [
-        'htmlWrapperStart' => 'LeadingSystems\Helpers\ContentHtmlWrapperStart',
-        'htmlWrapperStop' => 'LeadingSystems\Helpers\ContentHtmlWrapperStop',
-    ]
-);
-
-$GLOBALS['TL_WRAPPERS']['start'][] = 'htmlWrapperStart';
-$GLOBALS['TL_WRAPPERS']['stop'][] = 'htmlWrapperStop';
-
-if (TL_MODE === 'BE') {
+if (System::getContainer()->get('contao.routing.scope_matcher')->isBackendRequest(System::getContainer()->get('request_stack')->getCurrentRequest() ?? Request::create(''))) {
     $GLOBALS['LS_API_HOOKS']['apiReceiver_processRequest'][] = array('LeadingSystems\Helpers\ls_shop_apiController_findDoubleFlexContents', 'processRequest');
 }
