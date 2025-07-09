d19b806772e636be18b902d77def8fc7b13b72a8
Rainer Blechinger <rainer.blechinger@web.de>
2025-07-09 15:44:26 +0200

    rename funktion name

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -9,7 +9,7 @@ class PageController extends AbstractController
 {
     private static array $cache_pageModel = [];
 
-    public static function getPageDetails(int $pageId): ?PageModel {
+    public static function getPageDetailsCached(int $pageId): ?PageModel {
         if (!isset(self::$cache_pageModel[$pageId])) {
             self::$cache_pageModel[$pageId] = PageModel::findWithDetails($pageId);
         }
